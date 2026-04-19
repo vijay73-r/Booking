fc0e789a28961d8d4ceceea9bbef44990e00c887
vijay73-r <vijayr81234@gmail.com>
2026-04-19 12:18:59 +0530

    Property

---
+++ src/main/java/com/smartbooking/smart_booking/property/service/PropertyService.java
@@ -0,0 +1,10 @@
+package com.smartbooking.smart_booking.property.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PropertyService {
+
+}
